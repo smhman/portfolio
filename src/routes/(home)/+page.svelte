29d6dc512b7b07ae4eda5f3052e572e20cdf6e3e
sundei <sundei@sundei.ee>
2025-05-14 17:55:00 +0300

    Update +page.svelte

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -66,7 +66,7 @@
 	
 </p>
 <div style="margin-top: 20px; border-radius: 13px; overflow: hidden;">
-    <iframe title="Katarsis - Tavo akys | Spotify" src="https://open.spotify.com/embed/track/1imPa2lzxmLKggLCpkBAoo" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe title="Katarsis - Tavo akys | Spotify" src="https://open.spotify.com/embed/track/1imPa2lzxmLKggLCpkBAoo" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 </div>
 
 			
